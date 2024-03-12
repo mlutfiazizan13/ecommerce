4eb576d4e17837388cff71cb573d08d7f55b70a8
Muhamad Lutfi Azizan <mlutfiazizan@gmail.com>
2024-03-12 17:14:04 +0700

    init commit & setting welcome page + fontawesome

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,67 @@
+<script setup>
+import WebLayout from '@/Layouts/WebLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+import Glide from '@glidejs/glide';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    const glide = new Glide('.glide').mount();
+})
+
+</script>
+
+<template>
+
+    <!-- <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10">
+            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+
+            <template v-else>
+                <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
+
+                <Link v-if="canRegister" :href="route('register')" class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
+            </template>
+        </div>
+    </div> -->
+
+    <WebLayout title="Home">
+    
+        <section class="">
+            <div class="glide">
+                <div class="glide__track" data-glide-el="track">
+                  <ul class="glide__slides">
+                    <li class="glide__slide">
+                        <div class=" flex justify-center items-center bg-transparent h-[890px] cursor-default">
+                            <div class="relative max-w-[1200px] mx-auto">
+                                <div class=" grid grid-cols-2 gap-10 items-center">
+                                    <div class="w-full">
+                                        <p class="text-lg uppercase font-bold">New Product!</p>
+                                        <p class="text-8xl font-medium mb-10">PRODUCT</p>
+                                        <p class="text-sm opacity-70 mb-10">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque at cum provident, perspiciatis aspernatur incidunt repudiandae? Ducimus tempore vitae itaque.</p>
+                                        <a href="#" class="bg-black text-white py-2 px-5 text-lg">Check Now !</a>
+                                    </div>
+                                    <div>
+                                        <img src="https://htmldemo.net/payne/payne/assets/img/slider/slider-image-02.png" alt="">
+                                    </div>
+                                </div>
+
+                            </div>
+                           
+                        </div>
+                    </li>
+                    <li class="glide__slide">    
+                        <div class="bg-slate-500 h-[890px]">
+                    2
+                        </div>
+                    </li>
+                  </ul>
+                </div>
+                <div class="glide__bullets" data-glide-el="controls[nav]">
+                    <button class="glide__bullet border border-black" data-glide-dir="=0"></button>
+                    <button class="glide__bullet border border-black" data-glide-dir="=1"></button>
+                  </div>
+              </div>
+        </section>
+    </WebLayout>
+
+</template>
---
+++ resources/js/Shared/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { computed } from 'vue';
+import { Link } from '@inertiajs/vue3';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+// const props = defineProps({
+//     href: String,
+//     active: Boolean,
+// });
+
+// const classes = computed(() => {
+//     return props.active
+//         ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+//         : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+// });
+</script>
+
+<template>
+    <div class="relative max-w-[1200px] mx-auto">
+        <div class="absolute flex justify-between items-center bg-transparent w-full h-[100px] z-50">
+            <div class="">
+                <p class="text-3xl font-medium">Product</p>
+            </div>
+            <div class="flex gap-10 text-lg">
+                <p>Home</p>
+                <p>Producs</p>
+                <p>Contact Us</p>
+          
+            </div>
+            <div class="flex gap-5">
+                <div class="text-xl">
+                    <p>Login</p>
+                </div>
+                <div class="text-2xl">
+                    <font-awesome-icon :icon="['fas', 'heart']" />
+                </div>
+                <div class="text-2xl">
+                    <font-awesome-icon :icon="['fas', 'bag-shopping']" />
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
